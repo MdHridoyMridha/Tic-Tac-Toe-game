aae936b3c5725aed1d198791236f3b6beea455e0
Hriody Mridha <hridoyhs369@gmail.com>
2025-09-25 07:25:30 +0600

    some update

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-let div = document.querySelector("div");
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("reset-btn");
 
-console.log(div);
-let id = div.getAttribute("div");
+let trun = true;   //player X player O
+
+const winPattern=[
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+]   //this is a 2d arrey
+
+boxes.forEach(box)=>{
+    box.addEvent()
+}
