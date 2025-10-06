f61fc06e31a8c9708e327c720b815f41a8ff9f27
MD Hridoy Mridha <hridoyhs369@gmail.com>
2025-10-06 11:44:43 +0600

    code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("reset-btn");
+let resetBtn = document.querySelector("#reset-btn");
 
-let trun = true;   //player X player O
+let trun0 = true;   //player X player O
 
 const winPattern=[
 [0,1,2],
@@ -14,6 +14,24 @@ const winPattern=[
 [2,4,6]
 ]   //this is a 2d arrey
 
-boxes.forEach(box)=>{
-    box.addEvent()
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("box was clicked");
+        if(trun0){ //player O
+            box.innerText ="O";
+            trun0 =false;
+        }else{  //player X
+            box.innerText = "X";
+            trun0 = true;
+        }
+        box.ariaDisabled=true;
+
+        checkWinner();
+    })
+});
+
+const checkWinner=()=>{
+    for (let pattern of winPattern){
+        console.log(pattern[0],pattern[1],pattern[2]);
+    }
 }
